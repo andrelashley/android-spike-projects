bb89b633f9d0e1d46cb97832cee66046d5ea9223
André Lashley <andre.lashley@gmail.com>
2018-11-04 16:41:59 -0800

    Implement reading from the database

--- FirebaseDemo/app/src/main/java/com/connecteddeveloper/firebasedemo/MainActivity.java
+++ FirebaseDemo/app/src/main/java/com/connecteddeveloper/firebasedemo/MainActivity.java
@@ -1,12 +1,23 @@
 package com.connecteddeveloper.firebasedemo;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
+
+    FirebaseApp mApp;
+    FirebaseDatabase mDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,9 +36,26 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void initFirebase() {
+        mApp = FirebaseApp.getInstance();
+        mDatabase = FirebaseDatabase.getInstance(mApp);
     }
 
     private void readDatabaseData() {
-        
+        DatabaseReference ref = mDatabase.getReference("chatMessages");
+
+        ValueEventListener eventListener = new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                Log.e(TAG, "onDataChange: Snapshot received"
+                        + dataSnapshot.getChildren() + " key : " + dataSnapshot.getKey().toString()
+                + " value : " + dataSnapshot.getValue().toString());
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        };
+        ref.addValueEventListener(eventListener);
     }
 }
