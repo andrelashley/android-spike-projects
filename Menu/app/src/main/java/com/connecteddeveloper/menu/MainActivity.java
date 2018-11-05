f2c4b2e40e36fab601e4467d64ede25179ecad82
André Lashley <andre.lashley@gmail.com>
2018-11-04 23:38:37 -0800

    Implement a menu

---
+++ Menu/app/src/main/java/com/connecteddeveloper/menu/MainActivity.java
@@ -0,0 +1,37 @@
+package com.connecteddeveloper.menu;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.options_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+
+        if(id == R.id.menu_logout) {
+            Log.e(TAG, "Log out option selected");
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
